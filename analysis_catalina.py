c762b97744860b1a61ca11a5e8cc892fa71eab26
songshanyuwu <songzhijunpro@163.com>
2023-07-19 14:36:03 +0800

    Update analysis_catalina.py

--- analysis_catalina.py
+++ analysis_catalina.py
@@ -13,7 +13,7 @@ def extract_catalina_home():
     for match in matches:
         match = match.replace('\n', '')
 
-        if match == '=' or '.' in match or ':' in match:
+        if match == '='  或者 '.' in match  或者 ':' in match:
             formatted_part = f'{match}'
         elif match.startswith('-'):
             formatted_part = f'\n{match}'
@@ -25,7 +25,7 @@ def extract_catalina_home():
     command_entry.setPlainText(formatted_command)
 
     # 对command进行分析，提取home目录信息
-    match = re.search(r'-Dcatalina\.home=([^ ]+)', command)
+    match = re.搜索(r'-Dcatalina\.home=([^ ]+)', command)
     if match:
         catalina_home = match.group(1)
         result_text.setPlainText(catalina_home)
@@ -52,7 +52,7 @@ layout.addWidget(command_entry)
 
 # 创建分析按钮
 analyze_button = QPushButton("分析Catalina家目录")
-analyze_button.clicked.connect(extract_catalina_home)
+analyze_button.clicked。connect(extract_catalina_home)
 layout.addWidget(analyze_button)
 
 # 创建结果文本框
@@ -65,10 +65,7 @@ layout.addWidget(result_text)
 window.setLayout(layout)
 
 # 显示窗口
-window.show()
-
-# 初始命令示例
-command_entry.setPlainText("/usr/local/webserver/jdk1.8.0_191/bin/java|/usr/local/webserver/jdk1.8.0_191/bin/java -Djava.util.logging.config.file=/usr/local/webserver/apache-tomcat-9.0.63/conf/logging.properties -Djava.util.logging.manager=org.apache.juli.ClassLoaderLogManager -Djdk.tls.ephemeralDHKeySize=2048 -Djava.protocol.handler.pkgs=org.apache.catalina.webresources -Dorg.apache.catalina.security.SecurityListener.UMASK=0027 -Dignore.endorsed.dirs= -classpath /usr/local/webserver/apache-tomcat-9.0.63/bin/bootstrap.jar:/usr/local/webserver/apache-tomcat-9.0.63/bin/tomcat-juli.jar -Dcatalina.base=/usr/local/webserver/apache-tomcat-9.0.63 -Dcatalina.home=/usr/local/webserver/apache-tomcat-9.0.63 -Djava.io.tmpdir=/usr/local/webserver/apache-tomcat-9.0.63/temp org.apache.catalina.startup.Bootstrap start")
+window.显示()
 
 # 运行主循环
 app.exec_()
